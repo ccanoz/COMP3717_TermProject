c7d1285b724186e226314e4759e4824e05bb2342
Cyra <cyraa@live.ca>
2021-03-29 19:08:01 -0700

    Pull firbase data in account details page

--- app/src/main/java/com/bcit/termproject/AccountActivity.java
+++ app/src/main/java/com/bcit/termproject/AccountActivity.java
@@ -1,5 +1,6 @@
 package com.bcit.termproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -9,6 +10,13 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.NumberFormat;
 import java.util.HashMap;
 
 public class AccountActivity extends AppCompatActivity {
@@ -17,6 +25,12 @@ public class AccountActivity extends AppCompatActivity {
     View accountDetailGender;
     View accountDetailIncome;
     View currToggled;
+
+    TextView ageLabel;
+    TextView genderLabel;
+    TextView incomeLabel;
+
+    DatabaseReference dbUserInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +41,44 @@ public class AccountActivity extends AppCompatActivity {
         accountDetailAge = findViewById(R.id.account_detail_age);
         accountDetailGender = findViewById(R.id.account_detail_gender);
         accountDetailIncome = findViewById(R.id.account_detail_income);
+
+        ageLabel = accountDetailAge.findViewById(R.id.textView_accDetail_value);
+        genderLabel = accountDetailGender.findViewById(R.id.textView_accDetail_value);
+        incomeLabel  = accountDetailIncome.findViewById(R.id.textView_accDetail_value);
+
         setListeners();
         setFormInformation();
+
+        // TestUser TpBUQaOpqiP4wTykld49PrxD1m62
+        dbUserInfo = FirebaseDatabase.getInstance().getReference("user").child("TpBUQaOpqiP4wTykld49PrxD1m62");
+    }
+
+    /**
+     * Populate the activity layout with the scholarship details retrieved from
+     * the database.
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        dbUserInfo.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String age = snapshot.child("dob").getValue(String.class);
+                String gender = snapshot.child("gender").getValue(String.class);
+                String income = snapshot.child("yearlyIncome").getValue(String.class);
+
+                ageLabel.setText(age);
+                genderLabel.setText(gender);
+                incomeLabel.setText(income);
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     public void setFormInformation() {
