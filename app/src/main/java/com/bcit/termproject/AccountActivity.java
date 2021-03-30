7638ae77e08f295595a99ed829a8a9975a75b0c5
Cyra <cyraa@live.ca>
2021-03-29 19:59:17 -0700

    Account details page updating firebase data works

--- app/src/main/java/com/bcit/termproject/AccountActivity.java
+++ app/src/main/java/com/bcit/termproject/AccountActivity.java
@@ -7,17 +7,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import java.text.NumberFormat;
-import java.util.HashMap;
 
 public class AccountActivity extends AppCompatActivity {
 
@@ -29,6 +31,10 @@ public class AccountActivity extends AppCompatActivity {
     TextView ageLabel;
     TextView genderLabel;
     TextView incomeLabel;
+
+    Button saveAge;
+    Button saveGender;
+    Button saveIncome;
 
     DatabaseReference dbUserInfo;
 
@@ -46,6 +52,10 @@ public class AccountActivity extends AppCompatActivity {
         genderLabel = accountDetailGender.findViewById(R.id.textView_accDetail_value);
         incomeLabel  = accountDetailIncome.findViewById(R.id.textView_accDetail_value);
 
+        saveAge = accountDetailAge.findViewById(R.id.button_saveAccount);
+        saveGender = accountDetailGender.findViewById(R.id.button_saveAccount);
+        saveIncome = accountDetailIncome.findViewById(R.id.button_saveAccount);
+
         setListeners();
         setFormInformation();
 
@@ -53,8 +63,29 @@ public class AccountActivity extends AppCompatActivity {
         dbUserInfo = FirebaseDatabase.getInstance().getReference("user").child("TpBUQaOpqiP4wTykld49PrxD1m62");
     }
 
+    public void updateUser(String field, String updateValue) {
+
+        Log.d("Updating: ", field + " to " + updateValue);
+
+        Task<Void> setValueTask = dbUserInfo.child(field).setValue(updateValue);
+
+        setValueTask.addOnSuccessListener(new OnSuccessListener() {
+            @Override
+            public void onSuccess(Object o) {
+                Toast.makeText(AccountActivity.this, "Updated " + field, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        setValueTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(AccountActivity.this, "Something went wrong.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
     /**
-     * Populate the activity layout with the scholarship details retrieved from
+     * Populate the activity layout with the user details retrieved from
      * the database.
      */
     @Override
@@ -104,56 +135,71 @@ public class AccountActivity extends AppCompatActivity {
         Button editIncome = accountDetailIncome.findViewById(R.id.button_editAccount);
         editIncome.setOnClickListener(onClickEditIncome);
 
+        saveAge.setOnClickListener(onEditAge);
+        saveGender.setOnClickListener(onEditGender);
+        saveIncome.setOnClickListener(onEditIncome);
+    }
+
+    public View.OnClickListener onEditAge= new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            EditText editTextAccount = accountDetailAge.findViewById(R.id.editText_account);
+            String updateValue = editTextAccount.getText().toString().trim();
+            updateUser("dob", updateValue);
+        }
+    };
+
+    public View.OnClickListener onEditGender= new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            EditText editTextAccount = accountDetailGender.findViewById(R.id.editText_account);
+            String updateValue = editTextAccount.getText().toString().trim();
+            updateUser("gender", updateValue);
+        }
+    };
+
+    public View.OnClickListener onEditIncome= new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            EditText editTextAccount = accountDetailIncome.findViewById(R.id.editText_account);
+            String updateValue = editTextAccount.getText().toString().trim();
+            updateUser("yearlyIncome", updateValue);
+        }
+    };
+
+    public void setVisibility() {
+        LinearLayout linearLayout = currToggled.findViewById(R.id.linearLayout_account);
+        if (linearLayout.getVisibility() == View.GONE) { ;
+            linearLayout.setVisibility(View.VISIBLE);
+        } else {
+            linearLayout.setVisibility(View.GONE);
+        }
     }
 
     public View.OnClickListener onClickEditAge = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            int id = v.getId();
             currToggled = accountDetailAge;
-            Log.d("Clicked Button: ", String.valueOf(id));
-            LinearLayout linearLayout = currToggled.findViewById(R.id.linearLayout_account);
-            if (linearLayout.getVisibility() == View.GONE) {
-                Log.d("Visibility: ", "Set Visible");
-                linearLayout.setVisibility(View.VISIBLE);
-            } else {
-                linearLayout.setVisibility(View.GONE);
-                Log.d("Visibility: ", "Set Gone");
-            }
+            setVisibility();
         }
     };
 
     public View.OnClickListener onClickEditGender = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            int id = v.getId();
             currToggled = accountDetailGender;
-            Log.d("Clicked Button: ", String.valueOf(id));
-            LinearLayout linearLayout = currToggled.findViewById(R.id.linearLayout_account);
-            if (linearLayout.getVisibility() == View.GONE) {
-                Log.d("Visibility: ", "Set Visible");
-                linearLayout.setVisibility(View.VISIBLE);
-            } else {
-                linearLayout.setVisibility(View.GONE);
-                Log.d("Visibility: ", "Set Gone");
-            }
+            setVisibility();
         }
     };
 
     public View.OnClickListener onClickEditIncome = new View.OnClickListener() {
         @Override
         public void onClick(View v){
-            int id = v.getId();
             currToggled = accountDetailIncome;
-            Log.d("Clicked Button: ", String.valueOf(id));
-            LinearLayout linearLayout = currToggled.findViewById(R.id.linearLayout_account);
-            if (linearLayout.getVisibility() == View.GONE) {
-                Log.d("Visibility: ", "Set Visible");
-                linearLayout.setVisibility(View.VISIBLE);
-            } else {
-                linearLayout.setVisibility(View.GONE);
-                Log.d("Visibility: ", "Set Gone");
-            }
+            setVisibility();
         }
     };
 
