e55cac8460ad5e68b18f89e12e7947de3d3154b5
Cyra <cyraa@live.ca>
2021-04-05 00:45:28 -0700

    AccountActivity now opens a different dialog depending on which edit button is clicked

--- app/src/main/java/com/bcit/termproject/AccountActivity.java
+++ app/src/main/java/com/bcit/termproject/AccountActivity.java
@@ -10,7 +10,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,7 +23,6 @@ import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.datepicker.MaterialDatePicker;
-import com.google.android.material.datepicker.MaterialPickerOnPositiveButtonClickListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -36,6 +40,7 @@ public class AccountActivity extends AppCompatActivity {
     FirebaseUser currAuthUser;
     User currUser;
     TextView userName;
+    String employStatus;
 
     MaterialDatePicker<?> datePicker;
 
@@ -57,16 +62,24 @@ public class AccountActivity extends AppCompatActivity {
         currAuthUser = FirebaseAuth.getInstance().getCurrentUser();
         userName = findViewById(R.id.textView_userName_account);
 
-        Log.d("current user", currAuthUser.getUid());
-
         dbUserInfo = FirebaseDatabase.getInstance().getReference("user");
     }
 
+    /**
+     * Update a user detail field for the current user to the value passed in.
+     * @param field: a String, the user field to be updated in the database
+     * @param updateValue: a String, the new value to be set
+     */
     public void updateUser(String field, String updateValue) {
 
-        Log.d("Updating: ", field + " to " + updateValue);
+        Task<Void> setValueTask;
 
-        Task<Void> setValueTask = dbUserInfo.child(currAuthUser.getUid()).child(field).setValue(updateValue);
+        if (field.equals("employed")) {
+            boolean employment = updateValue.equals("Employed");
+            setValueTask = dbUserInfo.child(currAuthUser.getUid()).child(field).setValue(employment);
+        } else {
+           setValueTask = dbUserInfo.child(currAuthUser.getUid()).child(field).setValue(updateValue);
+        }
 
         setValueTask.addOnSuccessListener(new OnSuccessListener() {
             @Override
@@ -100,7 +113,6 @@ public class AccountActivity extends AppCompatActivity {
                 setAccountDetails();
             }
 
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -110,7 +122,7 @@ public class AccountActivity extends AppCompatActivity {
 
 
     /**
-     *
+     *  Set the RecyclerView to display user account details.
      */
     private void setAccountDetails() {
         accDetailList.clear();
@@ -129,7 +141,7 @@ public class AccountActivity extends AppCompatActivity {
         accDetailMap.put("yearlyIncome", currUser.getYearlyIncome());
         accDetailMap.put("nationality", currUser.getNationality());
 
-        String employStatus = currUser.getEmployed() ? "Employed" : "Unemployed";
+        employStatus = currUser.getEmployed() ? "Employed" : "Unemployed";
         accDetailMap.put("employed", employStatus);
 
         AccountAdapter adapter = new AccountAdapter(accDetailMap, accDetailList);
@@ -138,27 +150,26 @@ public class AccountActivity extends AppCompatActivity {
             @Override
             public void OnClick(String label) {
 
-                int formId;
-
                 // Show the corresponding layout for the label passed in
                 switch(label) {
                     case "DOB":
                         showDOBDialog();
                         return;
                     case "name":
+                        showFormDialog(label, currUser.getName());
+                        return;
                     case "yearlyIncome":
-                        formId = R.layout.account_detail_form;
-                        break;
+                        showFormDialog(label, currUser.getYearlyIncome());
+                        return;
                     case "gender":
+                        showSpinnerDialog(label, currUser.getGender());
+                        return;
                     case "nationality":
-                        formId = R.layout.account_detail_spinner;
-                        break;
+                        showSpinnerDialog(label, currUser.getNationality());
+                        return;
                     default:
-                        // This should be a radio button for employment status
-                        formId = R.layout.account_detail_form;
+                        showRadioDialog(label, employStatus);
                 }
-
-                showFormDialog(formId, label);
             }
         });
 
@@ -167,28 +178,95 @@ public class AccountActivity extends AppCompatActivity {
     }
 
     /**
-     * For name and income
+     * Displays a dialog with an EditText if the user chooses to edit their name or yearly income.
      */
-    private void showFormDialog(int formId, String label) {
+    private void showFormDialog(String label, String value) {
 
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        Log.d("formId", String.valueOf(formId));
-
-        LayoutInflater inflater = getLayoutInflater();
-
-        View dialogView = inflater.inflate(formId, null);
+        View dialogView = setDialogView(R.layout.account_detail_form, label, value);
         dialogBuilder.setView(dialogView);
 
-        TextView accDetail = dialogView.findViewById(R.id.textView_account);
-        TextView accValue = dialogView.findViewById(R.id.textView_accDetail_value);
-
         Button cancel = dialogView.findViewById(R.id.button_cancelEdit);
+        Button saveAccount = dialogView.findViewById(R.id.button_saveAccount);
 
-        accDetail.setText(label);
-        accValue.setText(currUser.getName());
+        EditText editTextAccount = dialogView.findViewById(R.id.editText_account);
 
         AlertDialog alertDialog = dialogBuilder.create();
         alertDialog.show();
+
+        saveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUser(label, editTextAccount.getText().toString());
+                alertDialog.dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    /**
+     * Set the values for the dialogView and Views that will be displayed in the dialogs
+     * for editing account details.
+     *
+     * @param formId: an int, the id of the form layout
+     * @param label: String label for the user field
+     * @param value: String value for the user field
+     * @return dialogView as a DialogView
+     */
+    private View setDialogView(int formId, String label, String value){
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(formId, null);
+
+        TextView accDetail = dialogView.findViewById(R.id.textView_account);
+        TextView accValue = dialogView.findViewById(R.id.textView_accDetail_value);
+        accDetail.setText(label);
+        accValue.setText(value);
+
+        return dialogView;
+    }
+
+    /**
+     * Display a spinner dialog if the user chooses to edit nationality or gender.
+     * @param label: String label for the user field
+     * @param value: String value for the user field
+     */
+    private void showSpinnerDialog(String label, String value) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        View dialogView = setDialogView(R.layout.account_detail_spinner, label, value);
+        dialogBuilder.setView(dialogView);
+
+        Button cancel = dialogView.findViewById(R.id.button_cancelEdit);
+        Button saveAccount = dialogView.findViewById(R.id.button_saveAccount);
+
+        Spinner spinnerForm = dialogView.findViewById(R.id.spinner_account);
+        String[] items;
+
+        if (label.equals("gender")) {
+            items = new String[]{"Male", "Female", "Other"};
+        } else {
+            items = new String[]{"Canadian", "American", "Other"};
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        spinnerForm.setAdapter(adapter);
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        saveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateUser(label, spinnerForm.getSelectedItem().toString());
+                alertDialog.dismiss();
+            }
+        });
 
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -200,12 +278,50 @@ public class AccountActivity extends AppCompatActivity {
 
 
     /**
+     * Display a dialog with radio buttons if the user chooses to edit their employment status.
+     * @param label: String label for the user field
+     * @param value: String value for the user field
+     */
+    private void showRadioDialog(String label, String value) {
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        View dialogView = setDialogView(R.layout.account_detail_radio, label, value);
+        dialogBuilder.setView(dialogView);
+
+        Button cancel = dialogView.findViewById(R.id.button_cancelEdit);
+        Button saveAccount = dialogView.findViewById(R.id.button_saveAccount);
+
+        RadioGroup radioGroup = dialogView.findViewById(R.id.radioGroup_employment);
+        int currChecked = currUser.getEmployed()? R.id.radioButton_employed: R.id.radioButton_unemployed;
+        radioGroup.check(currChecked);
+
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
+
+        saveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioButton selectedButton = dialogView.findViewById(radioGroup.getCheckedRadioButtonId());
+                updateUser(label, selectedButton.getText().toString());
+                alertDialog.dismiss();
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+    }
+
+    /**
      * Display a MaterialDatePicker for selecting Date of Birth.
      */
     private void showDOBDialog() {
         datePicker = MaterialDatePicker.Builder.datePicker()
                         .setInputMode(MaterialDatePicker.INPUT_MODE_TEXT)
-                        .setTitleText("Updating DOB")
+                        .setTitleText("Date of Birth: " + currUser.getDOB())
                         .build();
 
         datePicker.show(getSupportFragmentManager(), "tag");
